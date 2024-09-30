536d42ed92db5ff0ebb364e9d513fb532ee12685
Ali-humza <lexoff2023@gmail.com>
2024-09-30 20:15:35 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,31 @@
+console.log("i am a tutorial on loops")
+
+let a = 1
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+
+// }
+
+// let obj = {
+//     name: "ali",
+//     role: "programmer",
+//     company: "codewith..",
+
+
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(element)
+
+// }
+
+// for (const c of "harry") {
+//     console.log(c)
+// }
+
+let i=1;
+while(i<6){
+    console.log(i)
+    i++;
+}
